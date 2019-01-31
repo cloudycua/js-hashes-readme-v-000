0749186833b1e00d19a385399fcace218df3cc7e
Christine Cua <cloudycua@gmail.com>
2019-01-31 13:31:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-var playlist = { someone: 'something' };
+var playlist = { someone: 'something' }
 
 function updatePlaylist(playlist, artist, song){
-  playlist[artist] = song;
-  return playlist;
+  playlist[artist] = song
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artist){
-  delete[artist];
-  return playlist;
+  delete playlist[artist]
+  return playlist
 }
